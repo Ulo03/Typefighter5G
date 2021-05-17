4e2efc7798b862aecc45f708c3d209988d29220d
Watzum <emil.watz@gmail.com>
2021-05-17 16:48:42 +0200

    20210517 - Create Rooms

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const { RSA_NO_PADDING } = require('constants');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -18,8 +19,8 @@ let openGames = [];
 io.on("connection", function(socket) { // neue Verbindung eines Clients
   console.log(`Socket <${socket.id}> connected...`);
   allClients.push(socket);
-  socket.emit("gameUpdate", openGames);
-  socket.emit("onlineCount", allClients.length);
+  io.emit("gameUpdate", openGames);
+  io.emit("onlineCount", allClients.length);
   let si = -1;
   
   socket.on("sendUsername", function(message) {
@@ -28,13 +29,21 @@ io.on("connection", function(socket) { // neue Verbindung eines Clients
     console.log(allUsernames[si] + " logged in!");
   });
 
+  socket.on("createGame", function(roomName) {
+    console.log(roomName);
+    socket.join(roomName);
+    openGames.push(roomName);
+    io.emit("gameUpdate", openGames);
+    console.log("room created");
+  });
+
   socket.on("disconnect", function() {
     si = allClients.indexOf(socket);
     console.log(`Socket <${socket.id}> disconnected...`);
     console.log(allUsernames[si] + " logged out!");
     allClients.splice(si, 1);
     allUsernames.splice(si, 1);
-    socket.emit("onlineCount", allClients.length);
+    io.emit("onlineCount", allClients.length);
   });
 });
 
