f6ce0500c9d4dacf568ed9a0dc752fe317c5decb
Ulovec Lukas <uloveclukas@gmail.com>
2021-06-01 15:38:25 +0200

    make it better

--- client/src/components/Typefighter.jsx
+++ client/src/components/Typefighter.jsx
@@ -9,7 +9,7 @@ import Game from "./Game";
 function Typefighter(props) {
 
   useEffect(() => {
-    props.setSocket(io(props.socketURL));
+    props.setSocket(io());
   }, [props.socketURL]);
 
   useEffect(() => {
